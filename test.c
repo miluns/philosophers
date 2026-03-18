a211c3f398b595bcfa2780f8335e79413eba867f
Milosz Stawski <mstawski@c1r6s2.42warsaw.pl>
2026-03-18 14:38:01 +0100

    repo cleanup

--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int	main(void)
-{
-	int	i = 1;
-
-	while (i < 5)
-		printf("%d\n", ++i);
-}
--- tests/test3.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int	main(void)
-{
-	printf("Raz\n");
-	printf("%i\n", usleep(0));
-	printf("Dwa\n");
-}
